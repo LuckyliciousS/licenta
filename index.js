56920d3d85382b7aafab53b1f94a28bde74e3e85
LuckyliciousS <63809814+LuckyliciousS@users.noreply.github.com>
2022-06-01 13:50:35 +0300

    secondCommit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => {
 
-  res.send('Template server for NodeJS is online')
+  res.send('Template server for NodeJS is online but has some new attributes')
 })
 
 app.listen(port, () => {
